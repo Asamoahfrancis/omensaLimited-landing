8a449776cf257f2339fcb3eab4a2bf64e9490981
Derrick Ankrah <darhin97@gmail.com>
2025-08-03 07:57:21 +0000

    fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,6 @@ import CompanyStats from "../components/company-stats";
 import ProductsCatalog from "../components/products-catalog";
 import ServicesSection from "../components/services-section";
 import IndustriesSection from "../components/industries-section";
-// import TestimonialsSection from "../components/testimonials-section";
-// import CertificationsSection from "../components/certifications-section";
 import ContactSection from "../components/contact-section";
 import Footer from "../components/footer";
 import {useEffect} from "react";
